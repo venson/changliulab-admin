8c3b8688ea1801ca091acf925f0a0ba432979fb7
venson <venson.hunt@gmail.com>
2023-08-03 23:13:28 +0800

    [fix] fix AvatarR and Avatars compoent 1. change url field from 'src' to 'avatar' 2. add padding for name

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,42 +1,52 @@
 import React, { Fragment, memo } from "react";
 import { useRedirect } from "react-admin";
 
-interface AvatarProps{
-    src: string;
-    name: string;
-    path?: string;
-    id:string;
-
+interface AvatarProps {
+  avatar: string;
+  name: string;
+  path?: string;
+  id: string;
 }
 
-const AvatarR = memo(({src,name, path,id }:AvatarProps)=>{
-    const redirect = useRedirect()
-    const redirectPath = path?? `/member/${id}`
-    return <>
-        <div className="flex flex-wrap items-center text-center p-4" onClick={()=>redirect(redirectPath)}>
-            <img src={src} className="w-8 h-8 rounded-full" /> <span className="hidden lg:inline-flex">{name}</span>
-        </div>
+const AvatarR = memo(({ avatar, name, path, id }: AvatarProps) => {
+  const redirect = useRedirect();
+  const redirectPath = path ?? `/member/${id}`;
+  return (
+    <>
+      <div
+        className="flex flex-wrap items-center text-center p-4"
+        onClick={() => redirect(redirectPath)}
+      >
+        <img src={avatar} className="w-8 h-8 rounded-full" />{" "}
+        <span className="hidden lg:inline-flex pl-2">{name}</span>
+      </div>
     </>
-
-})
+  );
+});
 export default AvatarR;
 
-interface AvatarsProps{
-    avatars: AvatarProps[];
-
+interface AvatarsProps {
+  avatars: AvatarProps[];
 }
 // export const Avatars =({avatars }:AvatarsProps)=>{
 //     return(
 //     avatars.map((avatar) => <AvatarR key={avatar.id} src={avatar.src} name={avatar.name} id={avatar.id}/>)
-//     ) 
+//     )
 // }
-export const Avatars = memo(({avatars}: AvatarsProps) => {
+export const Avatars = memo(({ avatars }: AvatarsProps) => {
+  if (avatars && avatars.length > 0) {
     return (
-        <>
-            {avatars.map((avatar) => (
-                <AvatarR key={avatar.id} src={avatar.src} name={avatar.name} id={avatar.id} />
-            ))}
-        </>
+      <div className="flex">
+        {avatars.map((avatar) => (
+          <AvatarR
+            key={avatar.id}
+            avatar={avatar.avatar}
+            name={avatar.name}
+            id={avatar.id}
+          />
+        ))}
+      </div>
     );
+  }
+  return <></>;
 });
-
